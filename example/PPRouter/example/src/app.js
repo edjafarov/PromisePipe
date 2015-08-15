88cebf1daee42feb68d7f579fed2ef5e245d943c
Eldar Djafarov <djkojb@gmail.com>
2015-08-15 16:30:24 +0300

    update router POC

--- example/PPRouter/example/src/app.js
+++ example/PPRouter/example/src/app.js
@@ -3,7 +3,6 @@ import {Router} from "./router"
 import HistoryApiAdapter from  "../../adapters/HistoryApiAdapter"
 
 function mount(data){
-  console.log('mount11');
   document.getElementById('content').innerHTML = data;
 }
 var FrontendAdapter = HistoryApiAdapter(mount);
@@ -12,7 +11,7 @@ Router.use(FrontendAdapter);
 document.getElementById('content').onclick = function(e){
   e.preventDefault()
   e.stopPropagation();
-  if(e.target.nodeType == 1 && e.target.href.indexOf(document.location.origin) == 0 ) {//is a link
+  if(e.target.nodeType == 1 && e.target.href && e.target.href.indexOf(document.location.origin) == 0 ) {//is a link
     Router.router.transitionTo(e.target.pathname)
   }
 }
