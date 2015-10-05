6b8aeb3041d2809df4b58c62300fd1b49183e9f4
Eldar Djafarov <djkojb@gmail.com>
2015-10-05 21:09:36 +0300

    #22

--- tests/PromisePipe.multiple-transitions.spec.js
+++ tests/PromisePipe.multiple-transitions.spec.js
@@ -261,3 +261,163 @@ describe('PromisePipe when comes to chains from other env', function(){
 	})
 
 })
+
+
+xdescribe('PromisePipe can find its way through complex nested transition #22', function(){
+	/*
+	It is more complicated than I thought
+	the nested chain do not know about capabilities of outer chain
+	thus some protocol is required that will predict behavior of execution
+	and will build a path or something
+	*/
+	var PromisePipe = require('../src/PromisePipe')();
+	var PromisePipeServer = require('../src/PromisePipe')();
+	var PromisePipeWorker1 = require('../src/PromisePipe')();
+	var PromisePipeWorker2 = require('../src/PromisePipe')();
+	PromisePipeServer.setEnv('server');
+	PromisePipeWorker1.setEnv('worker1');
+	PromisePipeWorker2.setEnv('worker2');
+
+
+	var context = {};
+	var context1 = {};
+	var context2 = {};
+	var data1 = 1;
+	var data2 = 2;
+	var fn1 = sinon.stub();
+	var fn2 = sinon.stub();
+	var fn3 = sinon.stub();
+	var fn4 = sinon.stub();
+	var fn5 = sinon.stub();
+
+
+	fn1.withArgs(data1).returns(data1);
+	fn2.withArgs(data1).returns(data1);
+	fn3.withArgs(data1).returns(data1);
+	fn4.withArgs(data1).returns(data1);
+	fn5.withArgs(data1).returns(data1);
+
+
+	var serverSocketMock = new EventEmitter();
+	var clientSocketMock = new EventEmitter();
+	var worker1SocketMock = new EventEmitter();
+	var worker2SocketMock = new EventEmitter();
+
+	function sendToClient(message){
+		clientSocketMock.emit('message', JSON.stringify(message));
+	}
+
+	function sendToServer(message){
+		serverSocketMock.emit('message', JSON.stringify(message));
+	}
+
+	function sendToWorker1(message){
+		worker1SocketMock.emit('message', JSON.stringify(message));
+	}
+	function sendToWorker2(message){
+		worker2SocketMock.emit('message', JSON.stringify(message));
+	}
+
+
+	PromisePipe.envTransition('client', 'server', function(message){
+		sendToServer(message);
+		return PromisePipe.promiseMessage(message);
+	});
+
+	PromisePipeServer.envTransition('server', 'worker1', function(message){
+		sendToWorker1(message);
+		return PromisePipeServer.promiseMessage(message);
+	});
+
+	PromisePipeWorker1.envTransition('worker1', 'worker2', 'server');
+
+	PromisePipeServer.envTransition('server', 'worker2', function(message){
+		sendToWorker2(message);
+		return PromisePipeServer.promiseMessage(message);
+	});
+
+	clientSocketMock.on('message', function(message){
+		message = JSON.parse(message);
+		PromisePipe.execTransitionMessage(message);
+	});
+
+	serverSocketMock.on('message', function(message){
+		message = JSON.parse(message);
+		PromisePipeServer.execTransitionMessage(message).then(function(data){
+			message.data = data;
+			sendToClient(message);
+		});
+	});
+
+	worker1SocketMock.on('message', function(message){
+		message = JSON.parse(message);
+
+		PromisePipeWorker1.execTransitionMessage(message).then(function(data){
+			message.data = data;
+			sendToServer(message);
+		});
+	});
+	worker2SocketMock.on('message', function(message){
+		message = JSON.parse(message);
+
+		PromisePipeWorker2.execTransitionMessage(message).then(function(data){
+			message.data = data;
+			sendToServer(message);
+		});
+	});
+
+
+	fn2._env = 'server';
+	fn3._env = 'worker1';
+	fn4._env = 'worker2';
+	fn5._env = 'client';
+
+	var finish = sinon.spy();
+
+	var pipe = PromisePipe()
+			.then(fn1)
+			.then(fn2)
+			.then(fn3)
+			.then(fn4)
+			.then(fn5);
+
+	var pipeServer = PromisePipeServer()
+			.then(fn1)
+			.then(fn2)
+			.then(fn3)
+			.then(fn4)
+			.then(fn5);
+
+
+	var pipeWorker1 = PromisePipeWorker1()
+			.then(fn1)
+			.then(fn2)
+			.then(fn3)
+			.then(fn4)
+			.then(fn5);
+
+	var pipeWorker2 = PromisePipeWorker2()
+			.then(fn1)
+			.then(fn2)
+			.then(fn3)
+			.then(fn4)
+			.then(fn5);
+
+
+	describe("should work if pseudo nested", function(){
+		before(function(done){
+			pipe(data1, context2).then(finish);
+			done()
+		})
+		it('should not fail', function(){
+				sinon.assert.calledOnce(fn1);
+				sinon.assert.calledOnce(fn2);
+				sinon.assert.calledOnce(fn3);
+				sinon.assert.calledOnce(fn4);
+				sinon.assert.calledOnce(fn5);
+				sinon.assert.calledOnce(finish);
+				sinon.assert.calledWithExactly(finish, data1);
+		})
+	})
+
+})
